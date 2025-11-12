0feaa06dfbb2c7dac30c2ba1f4b8eac3278bb039
Moisantiagogr <20223tn055@utez.edu.mx>
2025-11-12 17:37:39 -0600

    feat: implement enemy damage when hit by projectile

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,15 +1,39 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Collider))]
 public class Bullet : MonoBehaviour
 {
     public Vector3 direction;
     public float speed = 20f;
     private float timer;
 
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        timer = 0f;
+        if (rb != null)
+        {
+            rb.linearVelocity = direction.normalized * speed;
+        }
+    }
+    private void OnDisable()
+    {
+        timer = 0f;
+        if (rb != null) rb.linearVelocity = Vector3.zero;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += direction * (speed * Time.deltaTime);
+        if (rb == null)
+        {
+            transform.position += direction.normalized * (speed * Time.deltaTime);
+        }
         timer += Time.deltaTime;
         if (timer >= 5f)
         {
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,44 +1,67 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-
-public class Turret : MonoBehaviour
-{
-    public GameObject bulletPrefab, turretPivot, turretBody, firePoint, bulletPool;
-    private List<Bullet> bullets = new();
-    
-    private void Awake()
-    {
-        for (int i = 0; i < 50; i++)
-        {
-            var instance = Instantiate(bulletPrefab, bulletPool.transform);
-            var bullet = instance.GetComponent<Bullet>();
-            bullets.Add(bullet);
-            instance.SetActive(false);
-        }
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    IEnumerator Start()
-    {
-        while (true)
-        {
-            var available = bullets.FirstOrDefault(x => !x.gameObject.activeInHierarchy);
-            if (available)
-            {
-                available.direction = firePoint.transform.up;
-                available.transform.position = firePoint.transform.position;
-                available.gameObject.SetActive(true);
-            }
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+// csharp
+        // File: `Assets/Scripts/Turret.cs`
+        using System.Collections;
+        using System.Collections.Generic;
+        using System.Linq;
+        using UnityEngine;
         
-    }
-}
+        public class Turret : MonoBehaviour
+        {
+            public GameObject bulletPrefab;
+            public GameObject firePoint;
+            public GameObject bulletPool;
+            private List<Bullet> bullets = new();
+            [SerializeField] private float bulletSpeed = 20f;
+            [SerializeField] private int poolSize = 50;
+            [SerializeField] private float fireInterval = 0.5f;
+        
+            private void Awake()
+            {
+                if (bulletPool == null)
+                {
+                    Debug.LogError("Asignar `bulletPool` en el Inspector.");
+                    return;
+                }
+        
+                if (bulletPrefab == null)
+                {
+                    Debug.LogError("Asignar `bulletPrefab` en el Inspector.");
+                    return;
+                }
+        
+                for (int i = 0; i < poolSize; i++)
+                {
+                    var instance = Instantiate(bulletPrefab, bulletPool.transform);
+                    var bullet = instance.GetComponent<Bullet>();
+                    if (bullet == null) bullet = instance.AddComponent<Bullet>();
+                    // opcional: asegurarse de que el prefab tenga DamageProjectile para aplicar daño
+                    if (instance.GetComponent<DamageProjectile>() == null)
+                    {
+                        instance.AddComponent<DamageProjectile>();
+                    }
+                    instance.SetActive(false);
+                    bullets.Add(bullet);
+                }
+            }
+        
+            IEnumerator Start()
+            {
+                while (true)
+                {
+                    var available = bullets.FirstOrDefault(x => !x.gameObject.activeInHierarchy);
+                    if (available != null && firePoint != null)
+                    {
+                        available.direction = firePoint.transform.up;
+                        available.transform.position = firePoint.transform.position;
+                        available.gameObject.SetActive(true);
+        
+                        Rigidbody rb = available.GetComponent<Rigidbody>();
+                        if (rb != null)
+                        {
+                            rb.linearVelocity = available.direction.normalized * bulletSpeed;
+                        }
+                    }
+                    yield return new WaitForSeconds(fireInterval);
+                }
+            }
+        }
