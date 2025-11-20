287c9123768257698ed0857b3e8a52d9a7043a36
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-11-20 14:45:41 -0600

    feat: add new main menu page and edit all files to implement the new MainMenu

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -12,6 +12,6 @@ public class GameOverMenu : MonoBehaviour
         {
             musicController.StopMusic();
         }
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("HomeScreen");
     }
 }
