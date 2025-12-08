f670c1747735a3c1c73bba43f5f263cc2ede8cbc
JayrGilGaliciaJimenez <20223tn052@utez.edu.mx>
2025-12-07 22:48:13 -0600

    fix:(GameOverMenu) Fix load Scene form HomeScree To MainMenu

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -12,6 +12,6 @@ public class GameOverMenu : MonoBehaviour
         {
             musicController.StopMusic();
         }
-        SceneManager.LoadScene("HomeScreen");
+        SceneManager.LoadScene("MainMenu");
     }
 }
